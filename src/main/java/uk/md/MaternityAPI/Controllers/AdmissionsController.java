cf5a450a51acaba7f46fc21c7183949125a93303
Damon-Martin <91668778+Damon-Martin@users.noreply.github.com>
2023-03-28 23:47:28 +0100

    Making Project Structure: Skeleton/Empty Functions (TDD: Tests First)

---
+++ src/main/java/uk/md/MaternityAPI/Controllers/AdmissionsController.java
@@ -0,0 +1,12 @@
+package uk.md.MaternityAPI.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdmissionsController {
+    @GetMapping("api/Admissions/DischargedQuick")
+    public String dischargedQuick() {
+        return ("");
+    }
+}
---
+++ src/main/java/uk/md/MaternityAPI/Controllers/PatientsController.java
@@ -0,0 +1,4 @@
+package uk.md.MaternityAPI.Controllers;
+
+public class PatientsController {
+}
