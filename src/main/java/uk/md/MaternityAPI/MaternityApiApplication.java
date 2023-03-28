99a3f311bff0164dbae99bc11766157c5fad08d4
Damon-Martin <91668778+Damon-Martin@users.noreply.github.com>
2023-03-28 23:32:27 +0100

    Initial commit

---
+++ src/main/java/uk/md/MaternityAPI/MaternityApiApplication.java
@@ -0,0 +1,13 @@
+package uk.md.MaternityAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaternityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaternityApiApplication.class, args);
+	}
+
+}
