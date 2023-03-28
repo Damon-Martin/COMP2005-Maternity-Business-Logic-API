8bf9e217ad67e86588331696097a350d37bea5ca
Damon-Martin <91668778+Damon-Martin@users.noreply.github.com>
2023-03-29 00:26:43 +0100

    Starting to make Unit Tests: Should Fail

---
+++ src/test/java/uk/md/MaternityAPI/UnitTests/AdmissionsControllerTest.java
@@ -0,0 +1,25 @@
+package uk.md.MaternityAPI.UnitTests;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdmissionsControllerTest {
+    // Initialising Tests: Reset Each Test to these defaults
+    @BeforeEach
+    void initRepeat() {
+    }
+
+    // Check if the output is JSON with Key Values
+    @Test
+    void dischargedQuickReturnsJSON() {
+        // Attempts to parse the Response to a HashTable using
+
+        // If: Can be parsed into a HashTable then it passes
+
+        // Else: Fails (Just a normal String)
+        fail();
+    }
+}
