62685913a42aeb23d89a5b89e9ac6ca4f5fc374d
Damon-Martin <91668778+Damon-Martin@users.noreply.github.com>
2023-04-20 01:19:52 +0100

    Swagger Added: Using Springfox

--- src/main/java/uk/md/MaternityAPI/MaternityApiApplication.java
+++ src/main/java/uk/md/MaternityAPI/MaternityApiApplication.java
@@ -2,13 +2,19 @@ package uk.md.MaternityAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 public class MaternityApiApplication {
-
+	// Swagger: http://localhost:8080/swagger-ui.html
+	// Port 8080: Set in Application Properties
 	public static void main(String[] args) {
-		// Port 8080: Set in Application Properties
 		SpringApplication.run(MaternityApiApplication.class, args);
 	}
-
 }
