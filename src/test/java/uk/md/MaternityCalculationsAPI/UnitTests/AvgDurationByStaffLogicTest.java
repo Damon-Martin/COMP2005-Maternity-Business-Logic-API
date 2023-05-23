b28e06002ea23a65d1d90b1794bd297656088c26
Damon Martin <damon.martin@students.plymouth.ac.uk>
2023-05-23 03:01:48 +0100

    Starting to write tests with corner cases

---
+++ src/test/java/uk/md/MaternityCalculationsAPI/UnitTests/AvgDurationByStaffLogicTest.java
@@ -0,0 +1,31 @@
+package uk.md.MaternityCalculationsAPI.UnitTests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AvgDurationByStaffLogicTest {
+    @Test
+    void test_check_normal_date_duration_calculator() {
+        Assertions.fail("Normal Date Duration Calc: Not Implemented");
+    }
+
+    // Corner Case
+    @Test
+    void test_check_max_date_duration_difference_calculator() {
+        Assertions.fail("Normal Date Duration Calc: Not Implemented");
+    }
+
+    // Corner Case
+    // using 1 Sec diff
+    @Test
+    void test_check_min_date_duration_difference_calculator_1() {
+        Assertions.fail("Normal Date Duration Calc: Not Implemented");
+    }
+    
+    // Corner Case
+    // using 1 ms diff
+    @Test
+    void test_check_min_date_duration_difference_calculator_2() {
+        Assertions.fail("Normal Date Duration Calc: Not Implemented");
+    }
+}
